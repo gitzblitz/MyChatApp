69e2fdb710e03ffd58abefd8d6e8f25f21755287
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-28 17:58:28 +0200

    Adding UI to settings activity and status activity

--- app/src/main/java/com/gitzblitz/mychatapp/activities/SettingsActivity.kt
+++ app/src/main/java/com/gitzblitz/mychatapp/activities/SettingsActivity.kt
@@ -1,13 +1,61 @@
 package com.gitzblitz.mychatapp.activities
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.gitzblitz.mychatapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
+
+  var mDatabase: DatabaseReference? = null
+  var mCurrentUser: FirebaseUser? = null
+  var mStorageReference: StorageReference? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_settings)
+
+    supportActionBar!!.title = "Settings"
+
+    //instantiate references
+
+
+    mCurrentUser = FirebaseAuth.getInstance().currentUser
+
+    var userID = mCurrentUser!!.uid
+    mDatabase = FirebaseDatabase.getInstance().reference
+        .child("Users")
+        .child(userID)
+
+    mDatabase!!.addValueEventListener(object : ValueEventListener {
+
+      override fun onDataChange(dataSnapshot: DataSnapshot) {
+        var displayName = dataSnapshot.child("display_name").value
+        var image = dataSnapshot.child("image").value
+        var userStatus = dataSnapshot.child("status").value
+        var thumbnail = dataSnapshot.child("thumb_image").value
+
+        settingsDisplayName.text = displayName.toString()
+        settingsDisplayStatus.text = userStatus.toString()
+
+      }
+
+      override fun onCancelled(databaseError: DatabaseError) {
+
+      }
+    })
+
+    settingBtnChangeImage.setOnClickListener { }
+
+    settingsBtnChangeStatus.setOnClickListener {
+      var intent = Intent(this, StatusActivity::class.java)
+      intent.putExtra("status", settingsDisplayStatus.text.toString().trim())
+      startActivity(intent)
+    }
   }
 }
---
+++ app/src/main/java/com/gitzblitz/mychatapp/activities/StatusActivity.kt
@@ -0,0 +1,13 @@
+package com.gitzblitz.mychatapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.gitzblitz.mychatapp.R
+
+class StatusActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_status)
+  }
+}
