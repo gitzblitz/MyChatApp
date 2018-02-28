fcea240f955648a0c47a27e72be13219dd9458c7
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-28 21:13:04 +0200

    Adding image cropping library and functionality to upload image to firebase cloud storage

--- app/src/main/java/com/gitzblitz/mychatapp/activities/SettingsActivity.kt
+++ app/src/main/java/com/gitzblitz/mychatapp/activities/SettingsActivity.kt
@@ -1,14 +1,24 @@
 package com.gitzblitz.mychatapp.activities
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.gitzblitz.mychatapp.R
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import com.theartofdev.edmodo.cropper.CropImage
+import id.zelory.compressor.Compressor
 import kotlinx.android.synthetic.main.activity_settings.*
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -67,6 +77,87 @@ class SettingsActivity : AppCompatActivity() {
   }
 
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+    if (requestCode == GALLERY_ID && resultCode == Activity.RESULT_OK) {
+      var image: Uri = data!!.data
+
+      CropImage.activity(image)
+          .setAspectRatio(1, 1)
+          .start(this)
+
+    }
+
+    if (requestCode === CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+      val result = CropImage.getActivityResult(data)
+      if (resultCode === Activity.RESULT_OK) {
+        val resultUri = result.uri
+
+        var userId = mCurrentUser!!.uid
+        var thumbFile = File(resultUri.path)
+
+        var thumbBitmap = Compressor(this)
+            .setMaxWidth(200)
+            .setMaxHeight(200)
+            .setQuality(65)
+            .compressToBitmap(thumbFile)
+
+        // now upload to firebase
+        //create byte array output stream
+        var byteArray = ByteArrayOutputStream()
+        thumbBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArray)
+
+        var thumbByteArray: ByteArray
+        thumbByteArray = byteArray.toByteArray()
+
+        var filePath = mStorageReference!!.child("chat_profile_images")
+            .child(userId + ".jpg")
+
+        // create another directory for thumb images
+
+        var thumbFilePath = mStorageReference!!.child("chat_profile_images")
+            .child("thumbs")
+            .child(userId + ".jpg")
+
+        filePath.putFile(resultUri)
+            .addOnCompleteListener { task: Task<UploadTask.TaskSnapshot> ->
+
+              if (task.isSuccessful) {
+                // get the picture url
+                var downloadUrl = task.result.downloadUrl.toString()
+
+                // uploads task
+                var uploadTask: UploadTask = thumbFilePath
+                    .putBytes(thumbByteArray)
+                uploadTask.addOnCompleteListener {
+
+                  task: Task<UploadTask.TaskSnapshot> ->
+
+                  var thumbUrl = task.result.downloadUrl.toString()
+
+                  if (task.isSuccessful) {
+                    var updateObject = HashMap<String, Any>()
+                    updateObject.put("image", downloadUrl)
+                    updateObject.put("thumb_image", thumbUrl)
+
+                    // save profile image
+
+                    mDatabase!!.updateChildren(updateObject).addOnCompleteListener { task: Task<Void> ->
+                      if (task.isSuccessful) {
+                        Toast.makeText(this, "Profile image saved", Toast.LENGTH_LONG).show()
+                      } else {
+
+                      }
+                    }
+
+                  } else {
+
+                  }
+                }
+              }
+            }
+      }
+    }
     super.onActivityResult(requestCode, resultCode, data)
+
   }
 }
