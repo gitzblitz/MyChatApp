e9f17a1ce30493997b9bbfedcba8bf39bd7deab6
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-28 14:15:53 +0200

    Checker for logged in user redirect to dashboard activity

--- app/src/main/java/com/gitzblitz/mychatapp/MainActivity.kt
+++ app/src/main/java/com/gitzblitz/mychatapp/MainActivity.kt
@@ -3,13 +3,36 @@ package com.gitzblitz.mychatapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+  var mAuth : FirebaseAuth? = null
+  var user: FirebaseUser? = null
+  var mAuthListener: FirebaseAuth.AuthStateListener? = null
+
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    mAuth = FirebaseAuth.getInstance()
+
+    mAuthListener = FirebaseAuth.AuthStateListener {
+      firebaseAuth: FirebaseAuth ->
+
+      user = firebaseAuth.currentUser
+
+      if (user != null){
+          // go to dashboard
+        startActivity(Intent(this, DashboardActivity::class.java))
+        finish()
+      }else{
+        Toast.makeText(this, "Not logged in", Toast.LENGTH_LONG).show()
+      }
+    }
 
     btnCreateAccount.setOnClickListener {
       startActivity(Intent(this, CreateAccountActivity::class.java))
@@ -17,6 +40,18 @@ class MainActivity : AppCompatActivity() {
 
     btnLogin.setOnClickListener {
       startActivity(Intent(this,LoginActivity::class.java))
+    }
+  }
+
+  override fun onStart() {
+    super.onStart()
+    mAuth!!.addAuthStateListener (mAuthListener!!)
+  }
+
+  override fun onStop() {
+    super.onStop()
+    if (mAuthListener != null){
+      mAuth!!.removeAuthStateListener(mAuthListener!!)
     }
   }
 }
