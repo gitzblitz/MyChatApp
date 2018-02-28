b7998b1b80236f05c4b5d2ca30ceadbb74ffcd85
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-28 16:20:46 +0200

    Adding menu items for settings and logout

--- app/src/main/java/com/gitzblitz/mychatapp/activities/DashboardActivity.kt
+++ app/src/main/java/com/gitzblitz/mychatapp/activities/DashboardActivity.kt
@@ -1,11 +1,15 @@
 package com.gitzblitz.mychatapp.activities
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.gitzblitz.mychatapp.R
 import com.gitzblitz.mychatapp.adapters.SectionPagerAdapter
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
@@ -27,8 +31,30 @@ class DashboardActivity : AppCompatActivity() {
     if (intent.extras != null) {
       var userName = intent.extras.get("name")
       Toast.makeText(this, "username " + userName, Toast.LENGTH_LONG).show()
-    } else {
-
     }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+     super.onCreateOptionsMenu(menu)
+
+    menuInflater.inflate(R.menu.main_menu, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+     super.onOptionsItemSelected(item)
+
+    if (item != null){
+      if (item.itemId == R.id.menuLogout){
+        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(this,MainActivity::class.java))
+      }
+
+      if (item.itemId == R.id.menuSettings){
+        //take users to settings activity
+        startActivity(Intent(this,SettingsActivity::class.java))
+      }
+    }
+    return true
   }
 }
