5a41434162d60b1cce60c39d66f888af4305bbb3
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-03-01 13:42:45 +0200

    adding profile & chat activity and dialog chooser to navigate to both

--- app/src/main/java/com/gitzblitz/mychatapp/adapters/UsersAdapter.kt
+++ app/src/main/java/com/gitzblitz/mychatapp/adapters/UsersAdapter.kt
@@ -1,12 +1,16 @@
 package com.gitzblitz.mychatapp.adapters
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.gitzblitz.mychatapp.R
+import com.gitzblitz.mychatapp.activities.ChatActivity
+import com.gitzblitz.mychatapp.activities.ProfileActivity
 import com.gitzblitz.mychatapp.models.Users
 import com.google.firebase.database.DatabaseReference
 import com.squareup.picasso.Picasso
@@ -15,7 +19,7 @@ import de.hdodenhof.circleimageview.CircleImageView
 /**
  * Created by george.ngethe on 28/02/2018.
  */
-class UsersAdapter(databaseQuery: DatabaseReference, var context: Context): FirebaseRecyclerAdapter<Users,UsersAdapter.ViewHolder>(Users::class.java,
+class UsersAdapter(databaseQuery: DatabaseReference, var context: Context) : FirebaseRecyclerAdapter<Users, UsersAdapter.ViewHolder>(Users::class.java,
     R.layout.users_row,
     UsersAdapter.ViewHolder::class.java,
     databaseQuery) {
@@ -25,20 +29,46 @@ class UsersAdapter(databaseQuery: DatabaseReference, var context: Context): Fire
     viewHolder!!.bindView(user!!, context)
 
     viewHolder.itemView.setOnClickListener {
-      //TODO create popup dialog where user can choose to see message or view profile
-      Toast.makeText(context, "User row clicked $userId", Toast.LENGTH_LONG).show()
+
+      //create an alert dialog
+      var options = arrayOf("Open Profile", "Send Message")
+      var builder = AlertDialog.Builder(context)
+      builder.setTitle("Select an Option")
+      builder.setItems(options, DialogInterface.OnClickListener { dialogInterace, i ->
+        var userName = viewHolder.userNameTxt
+        var userStatus = viewHolder.userStatusTxt
+        var profilePic = viewHolder.userProfilePicLink
+
+        if (i == 0) {
+          // open profile
+          var profileIntent = Intent(context, ProfileActivity::class.java)
+          profileIntent.putExtra("userId", userId)
+          context.startActivity(profileIntent)
+        } else {
+          // send message
+          var chatIntent = Intent(context, ChatActivity::class.java)
+          chatIntent.putExtra("userId", userId)
+          chatIntent.putExtra("name", userName)
+          chatIntent.putExtra("status", userStatus)
+          chatIntent.putExtra("profile", profilePic)
+
+          context.startActivity(chatIntent)
+
+        }
+      })
+      builder.show()
     }
   }
 
 
-   class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+  class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     var userNameTxt: String? = null
     var userStatusTxt: String? = null
     var userProfilePicLink: String? = null
 
 
-    fun bindView(user: Users, context: Context){
+    fun bindView(user: Users, context: Context) {
       var userName = itemView.findViewById<TextView>(R.id.userName)
       var userStatus = itemView.findViewById<TextView>(R.id.userStatus)
       var userProfilePic = itemView.findViewById<CircleImageView>(R.id.usersProfileImageView)
