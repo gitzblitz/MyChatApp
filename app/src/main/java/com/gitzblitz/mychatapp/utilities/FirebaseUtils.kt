09eb4838a2239c6b7675b2c09320a0f17777c059
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-03-05 12:28:29 +0200

    Firebase utils class

---
+++ app/src/main/java/com/gitzblitz/mychatapp/utilities/FirebaseUtils.kt
@@ -0,0 +1,13 @@
+package com.gitzblitz.mychatapp.utilities
+
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+
+/**
+ * Created by george.ngethe on 02/03/2018.
+ */
+class FirebaseUtils {
+
+  var databaseReference: DatabaseReference? = null
+  var firebaseUser: FirebaseUser? = null
+}
